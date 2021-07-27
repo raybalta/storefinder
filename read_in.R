11dd737e13d9942d50cc596e76794db6fade20d2
Rbaltaza <raymund.baltazar@gmail.com>
2021-07-27 15:14:20 +1200

    added clean up function

--- read_in.R
+++ read_in.R
@@ -5,4 +5,22 @@ grep(".csv", files, value = T)
 
 foodstuff<-read.csv(paste0(data,"Scanned vs Not Scanned Matrix Grid TOTAL FOODSTUFFS All Categories Harraways  and  Sons Ltd_Scanned Matrix.csv"), skip = 5)
 supervalue<-read.csv(paste0(data, "Scanned vs Not Scanned Matrix Grid TOTAL SUPERVALUEFRESH CHOICE All Categories Harraways  and  Sons _Scanned Matrix.csv"), skip = 5)
-pak<-read.csv(paste0(data, "Scanned vs Not Scanned Matrix Grid TOTAL FOODSTUFFS All Categories Harraways  and  Sons Ltd_Scanned Matrix.csv"), skip = 5)
+woolwoorth<-read.csv(paste0(data, "Scanned vs Not Scanned Matrix Grid WOOLWORTHS NZ EXCL SVFC All Categories Harraways  and  Sons Ltd_Scanned Matrix.csv"), skip = 5)
+
+clean_source_data<-function(data){
+
+data<-data%>%
+  select(-State)%>%
+  filter(!is.na(Storeid))%>%
+  replace(is.na(.),0)%>%
+  gather(Product, count, -StoreName, -Storeid)
+
+
+return(data)
+
+
+}
+
+food_stuff<-clean_source_data(foodstuff)
+super<-clean_source_data(supervalue)
+countdown<-clean_source_data(woolwoorth)
